a3539c2c5c1cff0a45b1e3acab99b5796bc797ea
JoaSt96 <joaquin.stok@hotmail.com>
2026-05-28 13:24:32 -0300

    feat: enable app modal provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,35 @@
 import {QueryClientProvider} from "@tanstack/react-query"
-import {Stack} from "expo-router/stack"
+import {BottomSheetModalProvider} from "@gorhom/bottom-sheet"
+import {Stack} from "expo-router"
+import {GestureHandlerRootView} from "react-native-gesture-handler"
 import {KeyboardProvider} from "react-native-keyboard-controller"
 
 import "../global.css"
 
+import {ModalProvider} from "@/components/modals"
 import {queryClient} from "@/config/query.config"
 
 export default function Layout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <KeyboardProvider>
-        <Stack
-          screenOptions={{
-            headerLargeTitle: false,
-            headerShadowVisible: false,
-          }}
-        >
-          <Stack.Screen name="index" options={{title: "Markets"}} />
-          <Stack.Screen name="portfolio" options={{title: "Portfolio"}} />
-          <Stack.Screen name="search" options={{title: "Search"}} />
-        </Stack>
-      </KeyboardProvider>
-    </QueryClientProvider>
+    <GestureHandlerRootView className="flex-1">
+      <QueryClientProvider client={queryClient}>
+        <KeyboardProvider>
+          <BottomSheetModalProvider>
+            <ModalProvider>
+              <Stack
+                screenOptions={{
+                  headerLargeTitle: false,
+                  headerShadowVisible: false,
+                }}
+              >
+                <Stack.Screen name="index" options={{title: "Markets"}} />
+                <Stack.Screen name="portfolio" options={{title: "Portfolio"}} />
+                <Stack.Screen name="search" options={{title: "Search"}} />
+              </Stack>
+            </ModalProvider>
+          </BottomSheetModalProvider>
+        </KeyboardProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   )
 }
